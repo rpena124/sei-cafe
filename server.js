b2618460aaa3249982f311c7d43c32a914425640
rpena124 <rp1429@nyu.edu>
2022-12-21 19:25:09 -0500

    all files from mern-infra

--- server.js
+++ server.js
@@ -1,30 +1,38 @@
+// /server.js
 require('dotenv').config()
+require('./config/database');
 const express = require('express')
-//facicon will need path
 const path = require('path')
 const favicon = require('serve-favicon')
 const logger = require('morgan')
 const PORT = process.env.PORT || 3001
 
-const app = express() 
+const app = express()
 
-//req.body
-app.use(express.json())
+app.use(express.json())// req.body
+app.use((req, res, next) => {
+    res.locals.data = {}
+    next()
+})
 app.use(logger('dev'))
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico' )))
 app.use(express.static(path.join(__dirname, 'build')))
 
+app.use(require('./config/checkToken'))
 /*
-    app.use('/api', routes) <=== finish code once you got it
- */
+app.use('/api', routes) <====== Finish code once you got it
+*/
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/fruits', require('./routes/api/fruits'))
+
 app.get('/api/test', (req, res) => {
     res.json({'eureka': 'you have found it'})
 })
 
-app.get('*',(req, res)=>{
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`I am listening on ${PORT}`)
 })
---
+++ src/pages/OrderHistoryPage/OrderHistoryPage.js
@@ -0,0 +1,3 @@
+export default function OrderHistoryPage(props){
+    return(<h1>Order History Page</h1>)
+}
